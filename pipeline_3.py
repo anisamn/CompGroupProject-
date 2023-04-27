b34d0b67896a6fd3af5e79ed5d2c503be302752e
Sid Mehrotra <smehrotra1@luc.edu>
2023-04-27 09:33:01 +0000

    Clustering of MYBPHL now works, although not great

--- pipeline_3.py
+++ pipeline_3.py
@@ -1,10 +1,11 @@
 #This is the outline for Pipeline 3
 #Set up environment
 import cellPull
+import clusteringAnalysis_GOI
 #Ask for user input
 interest_gene = input("Enter gene of interest: ")
 #Pull all cells of indicated the indicated gene
 cellPull.pullCells(interest_gene)
 
 #perform the clustering on the cells containing gene of interest
-clusteringAnalysis_MYBPHL.performClustering()
+clusteringAnalysis_GOI.performClustering()
